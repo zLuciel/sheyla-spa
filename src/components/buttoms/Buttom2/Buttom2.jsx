aad87335811ebd13970d8e46bf11ebe2afca440e
zLuciel <emanuelabregu1999@gmail.com>
2022-09-03 22:24:31 -0500

    esto por hoy creo
    se

---
+++ src/components/buttoms/Buttom2/Buttom2.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import "./Buttom2.css"
+export const Buttom2 = () => {
+  return (
+    <button className='btn2'> Sobre mi
+    </button>
+  )
+}
--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,16 +1,26 @@
-import React from "react";
+//import {useState,useEffect} from "react";
+import { Buttom2 } from "../buttoms/Buttom2/Buttom2";
+import BtSobremi from "../buttoms/sobremi/BtSobremi";
 import "./Main.css";
 const mainspa = require.context(`../../assets/main-spa`, true);
 const Main = () => {
+
+
+
   return (
     <main className="main-principal-grid">
-      <img src={mainspa(`./main1.jpg`)} alt="spa" className="imagen-main" />
+      {/*<img src={mainspa(`./main1.jpg`)} alt="spa" className="imagen-main" />*/}
+      <video src={mainspa("./masaje.mp4")} className="imagen-main2" autoPlay muted loop></video>
       <div className="main-info">
         <h1>La mejor experiencia de belleza <br /> con <u> Sheyla Maza</u></h1>
         <p>
-          Con más de 10 años de experiencia Certificada, no dude en realizar
+          Con más de <b>10 años de experiencia</b>  Certificada, no dude en realizar
           consultas.
         </p>
+        <div className="container-buttom-main">
+          <BtSobremi/>
+          <Buttom2/>
+        </div>
       </div>
       <div className="main-bolitas">
         <div className="bol-1"></div>
